e1c94ea1ca619c793fc986d1d87ac285854c9022
Rick Brown <ricky.brown.00@gmail.com>
2021-02-08 17:44:30 +0000

    Initial Commit

---
+++ christmas-countdown/christmas_countdownApp.swift
@@ -0,0 +1,17 @@
+//
+//  christmas_countdownApp.swift
+//  christmas-countdown
+//
+//  Created by Rick Brown on 08/02/2021.
+//
+
+import SwiftUI
+
+@main
+struct christmas_countdownApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
